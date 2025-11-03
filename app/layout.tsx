c35f70e8333b7a820780579af562dd76f31fb468
ivedantmistry <ivedantmistry@gmail.com>
2025-11-03 23:32:37 +0530

    removed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Hush: A Data-Driven Entrepreneurial Experiment",
   description: "A case study in Lean Startup and market validation.",
-
-  // --- Updated this line ---
-  icons:
-    "https://res.cloudinary.com/dcwzstwur/image/upload/v1762184373/HUSH_tb0whs.png",
 };
 
 export default function RootLayout({
